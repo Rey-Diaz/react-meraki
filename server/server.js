8cee4bc51bde57519282d84c66f71577892147cd
Rey Diaz <137558428+Rey-Diaz@users.noreply.github.com>
2023-11-22 10:13:57 -0500

    working code
    sends emails and changelogs

--- server/server.js
+++ server/server.js
@@ -1,82 +1,134 @@
-// server.js
-
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
+const nodemailer = require('nodemailer');
 const app = express();
-const port = process.env.PORT || 3001; // Use the provided PORT or default to 3001
+const port = process.env.PORT || 3001;
 
-// Load environment variables from .env file
 require('dotenv').config();
 
-// Configure CORS to allow requests from your client application's origin
+// Setting up CORS with specific origin and methods
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with the actual origin of your client app
-  methods: 'GET', // Add other HTTP methods if needed
+  origin: 'http://localhost:5173',
+  methods: 'GET',
 }));
 
-// Function to make the API call for uplink statuses
-async function fetchMerakiData() {
+// Fetch network data from Meraki API
+async function fetchNetworkData() {
   try {
     const response = await axios.get(
-      `https://api.meraki.com/api/v1/organizations/${process.env.ORG_ID}/uplinks/statuses`,
-      {
-        headers: {
-          'X-Cisco-Meraki-API-Key': process.env.MERAKI_API_KEY,
-        },
-      }
+      `https://api.meraki.com/api/v1/organizations/${process.env.ORG_ID}/networks`,
+      { headers: {'X-Cisco-Meraki-API-Key': process.env.MERAKI_API_KEY} }
     );
     return response.data;
+  } catch (error) {
+    console.error('Error fetching network data:', error);
+    return null;
+  }
+}
+
+// Fetch and enrich Meraki data
+async function fetchMerakiData() {
+  try {
+    const [uplinkData, networkData] = await Promise.all([
+      axios.get(
+        `https://api.meraki.com/api/v1/organizations/${process.env.ORG_ID}/uplinks/statuses`,
+        { headers: {'X-Cisco-Meraki-API-Key': process.env.MERAKI_API_KEY} }
+      ),
+      fetchNetworkData()
+    ]);
+
+    // Enrich uplink data with network names
+    const enrichedUplinkData = uplinkData.data.map(uplink => {
+      const network = networkData.find(n => n.id === uplink.networkId);
+      return { ...uplink, networkName: network ? network.name : 'Unknown' };
+    });
+
+    console.log("Enriched Uplink Data:", enrichedUplinkData);
+    return { uplinkData: enrichedUplinkData, networkData };
   } catch (error) {
     console.error('Error fetching Meraki data:', error);
     return null;
   }
 }
 
-// Function to fetch network names
-async function fetchNetworkNames() {
+// Endpoint to get enriched Meraki data
+app.get('/meraki-data', async (req, res) => {
   try {
-    const networksResponse = await axios.get(
-      `https://api.meraki.com/api/v1/organizations/${process.env.ORG_ID}/networks`,
-      {
-        headers: {
-          'X-Cisco-Meraki-API-Key': process.env.MERAKI_API_KEY,
-        },
-      }
-    );
-    return networksResponse.data;
+    const enrichedData = await fetchMerakiData();
+    res.json(enrichedData.uplinkData);
   } catch (error) {
-    console.error('Error fetching network names:', error);
-    return null;
+    console.error('Error in /meraki-data route:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
-}
+});
 
+// Variables for tracking API calls
 let apiCallCounter = 0;
 let lastApiCallTime = null;
 
-// Set up the interval to call the API every 2 minutes (120000 milliseconds)
+// Periodic task to fetch Meraki data
 setInterval(async () => {
-  const uplinkData = await fetchMerakiData();
-  const networkNames = await fetchNetworkNames();
-
-  if (uplinkData && networkNames) {
-    const mergedData = uplinkData.map(device => {
-      const network = networkNames.find(net => net.id === device.networkId);
-      return { ...device, networkName: network ? network.name : 'Unknown' };
-    });
-
-    compareUplinkData(mergedData);
+  const enrichedData = await fetchMerakiData();
+  if (enrichedData) {
+    compareUplinkData(enrichedData.uplinkData, enrichedData.networkData);
     apiCallCounter++;
-    lastApiCallTime = new Date(); // Record the timestamp
+    lastApiCallTime = new Date();
   }
-  // TODO: Add any additional logic you need here
 }, 60000);
 
-let lastApiResponse = null; // Variable to store the last API response
-let changeLog = []; // Array to log changes
+// Variables for storing API response and change log
+let lastApiResponse = null;
+let changeLog = [];
+
+// Email sending function using Nodemailer with Mailtrap SMTP
+async function sendEmail(subject, body) {
+  const transporter = nodemailer.createTransport({
+    host: 'sandbox.smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to: process.env.EMAIL_TO,
+    subject: subject,
+    text: body
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+}
+
+// Function to format change log entry
+function formatChangeLogEntry(entry) {
+  return `Network ID: ${entry.networkId}\n` +
+         `Network Name: ${entry.networkName}\n` +
+         `Device Serial: ${entry.serial}\n` +
+         `Interface: ${entry.interface}\n` +
+         `Old Status: ${entry.oldStatus}\n` +
+         `New Status: ${entry.newStatus}\n` +
+         `Timestamp: ${entry.timestamp}`;
+}
+
+// Function to determine if a change should be logged
+function shouldLogChange(deviceSerial) {
+  const recentChange = changeLog.find(entry => entry.serial === deviceSerial && new Date() - new Date(entry.timestamp) < 300000);
+  return !recentChange;
+}
 
 // Function to compare uplink data
-function compareUplinkData(currentData) {
+function compareUplinkData(currentData, networkData) {
   if (!lastApiResponse) {
     lastApiResponse = currentData;
     return;
@@ -87,34 +139,38 @@ function compareUplinkData(currentData) {
     if (lastDevice) {
       device.uplinks.forEach(uplink => {
         const lastUplink = lastDevice.uplinks.find(u => u.interface === uplink.interface);
-        if (lastUplink && lastUplink.status !== uplink.status) {
-          // Log the change
-          changeLog.push({
+        if (lastUplink && lastUplink.status === 'active' && uplink.status !== 'active' && shouldLogChange(device.serial)) {
+          const changeLogEntry = {
             networkId: device.networkId,
-            networkName: device.networkName, // Include network name in the log
+            networkName: networkData.find(n => n.id === device.networkId)?.name || 'Unknown',
             serial: device.serial,
             interface: uplink.interface,
             oldStatus: lastUplink.status,
             newStatus: uplink.status,
             timestamp: new Date().toISOString()
-          });
+          };
+
+          changeLog.push(changeLogEntry);
+          sendEmail("Device Status Change Alert", formatChangeLogEntry(changeLogEntry));
         }
       });
     }
   });
 
-  // Update the lastApiResponse for the next comparison
   lastApiResponse = currentData;
 }
 
-// Endpoint to get the change log
+// Endpoint to retrieve the change log
 app.get('/change-log', (req, res) => {
   res.json(changeLog);
 });
+
+// Endpoint to retrieve API call count and last sync time
 app.get('/api-call-count', (req, res) => {
   res.json({ count: apiCallCounter, lastSync: lastApiCallTime });
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
